d2905d7472c368d56c25996bd86d5f96b3be6ed5
Victor Kweka <117171668+victorjudysen@users.noreply.github.com>
2024-02-11 00:57:56 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+//learning how to create a class and accessing members(attributes of the class) using an object.
+
+public class Main 
+{
+    int x = 10;
+
+    public static void main(String[] args)
+    {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    }
+}
+
