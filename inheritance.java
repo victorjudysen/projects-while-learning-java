a6a3b9c5f8658616a72eeb72fe4410b9da6688bf
Victor Kweka <117171668+victorjudysen@users.noreply.github.com>
2024-02-11 12:14:56 +0300

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,25 @@
+class Vehicle 
+{
+    protected String brand = "Ford"; //vehicle attribute
+    public void honk() //vehicle method
+    {
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+class Car extends Vehicle 
+{
+    private String modelName = "Mustang"; //car attribute
+    
+    public static void main(String[] args)
+    {
+        //create a myCar object
+        Car myCar = new Car();
+
+        //call the honk() method (from the Vehicle class) on the myCar object
+        myCar.honk();
+
+        //display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class            
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }            
+}
