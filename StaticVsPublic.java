4e1f13e3f1a6337a5c9d61e54f7cadbe30c3ddc7
Victor Kweka <117171668+victorjudysen@users.noreply.github.com>
2024-02-24 13:12:03 +0300

    Update StaticVsPublic.java

--- StaticVsPublic.java
+++ StaticVsPublic.java
@@ -1,20 +1,23 @@
 public class StaticVsPublic
 {
+    //static method
     static void myStaticMethod()
     {
         System.out.println("Static methods can be called without creating objects");
     }
 
+    //public method
     public void myPublicMethod()
     {
         System.out.println("Public methods must be called by creating objects");
     }
 
+    //main method
     public static void main(String[] x)
     {
-        myStaticMethod();
+        myStaticMethod(); //call the static method
 
-        StaticVsPublic myObj = new StaticVsPublic();
-        myObj.myPublicMethod(); 
+        StaticVsPublic myObj = new StaticVsPublic(); //create an object of StaticVsPublic Class
+        myObj.myPublicMethod(); //call the public method on the object
     }    
 }
