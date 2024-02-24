5ee87114544dd6e96fd0fa8af79a56b6eea9c127
Victor Kweka <117171668+victorjudysen@users.noreply.github.com>
2024-02-24 11:31:40 +0300

    code to describe functionality of two-dimensional Array

---
+++ Dim2Array.java
@@ -0,0 +1,14 @@
+public class Dim2Array
+{
+    public static void main(String[] args)
+    {
+        int[][] myNumbers = {{1,2,3,4},{5,6,7,8}};
+        for(int i=0; i < myNumbers.length; ++i)
+        {
+            for(int j=0; j < myNumbers[i].length; ++j)
+            {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+    }
+}
